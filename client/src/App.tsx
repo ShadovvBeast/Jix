317385ca47ba5d58d4a96e45c24ec23dce594b27
SB <asaf@sb0.tech>
2025-12-06 12:14:55 +0200

    feat: add React client with Vite setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Jix - Multiplayer Quiz Game</h1>
+      <p>Welcome to Jix!</p>
+    </div>
+  );
+}
+
+export default App;
